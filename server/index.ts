4a29eb743d97509b0c96a88267bc036301df9aaa
Liberty Takehara <libertytakehara@Libertys-MacBook-Air.local>
2024-05-18 16:55:51 +0900

    update

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,38 @@ import express from 'express'
 const app: express.Express = express()
 const port = 3000
 
+let body: any = null;
+
+/**
+ * health check
+ */
 app.get('/', (_req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World from M5E server!');
 })
 
+/**
+ * clientに次に実行してもらいたい命令を取得するための口。
+ */
+app.get('/order', (_req, res) => {
+  res.send('Hello World from M5E server!');
+})
+
+/**
+ * clientが、画像をアップロードする口
+ */
+app.post('/photo', (req, res) => {
+  body = req.body;
+  console.log('body: ' + body);
+})
+
+/**
+ * アップロードした画像を、人間がブラウザから見るための口
+ */
+app.get('/photo', (req, res) => {
+  console.log('body: ' + body);
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
